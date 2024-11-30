c21377dadb0033d52038fa0fce3ba1da65af315c
4Source <my.log.in.dennis@gmail.com>
2024-11-30 17:52:35 +0100

    Lightning

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,64 @@
 #include "Camera.h"
 
 // Vertices coordinates
-GLfloat vertices[] =
-    { //     COORDINATES     /        COLORS      /   TexCoord  //
-        -0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
-        -0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
-        0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
-        0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
-        0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f};
+GLfloat vertices[] = {
+    //     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
+    -0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f,  // Bottom side
+    -0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 5.0f, 0.0f, -1.0f, 0.0f, // Bottom side
+    0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 5.0f, 0.0f, -1.0f, 0.0f,  // Bottom side
+    0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f, 0.0f, -1.0f, 0.0f,   // Bottom side
+
+    -0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, -0.8f, 0.5f, 0.0f,  // Left Side
+    -0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f, -0.8f, 0.5f, 0.0f, // Left Side
+    0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f, -0.8f, 0.5f, 0.0f,   // Left Side
+
+    -0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f, 0.0f, 0.5f, -0.8f, // Non-facing side
+    0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, 0.0f, 0.5f, -0.8f,  // Non-facing side
+    0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f, 0.0f, 0.5f, -0.8f,   // Non-facing side
+
+    0.5f, 0.0f, -0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, 0.8f, 0.5f, 0.0f, // Right side
+    0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f, 0.8f, 0.5f, 0.0f,  // Right side
+    0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f, 0.8f, 0.5f, 0.0f,  // Right side
+
+    0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 5.0f, 0.0f, 0.0f, 0.5f, 0.8f,  // Facing side
+    -0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, 0.0f, 0.5f, 0.8f, // Facing side
+    0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f, 0.0f, 0.5f, 0.8f   // Facing side
+};
 
 // Indices for vertices order
-GLuint indices[] =
-    {
-        0, 1, 2,
-        0, 2, 3,
-        0, 1, 4,
-        1, 2, 4,
-        2, 3, 4,
-        3, 0, 4};
+GLuint indices[] = {
+    0, 1, 2,    // Bottom side
+    0, 2, 3,    // Bottom side
+    4, 6, 5,    // Left side
+    7, 9, 8,    // Non-facing side
+    10, 12, 11, // Right side
+    13, 15, 14  // Facing side
+};
+
+GLfloat lightVertices[] = {
+    //     COORDINATES     //
+    -0.1f, -0.1f, 0.1f,
+    -0.1f, -0.1f, -0.1f,
+    0.1f, -0.1f, -0.1f,
+    0.1f, -0.1f, 0.1f,
+    -0.1f, 0.1f, 0.1f,
+    -0.1f, 0.1f, -0.1f,
+    0.1f, 0.1f, -0.1f,
+    0.1f, 0.1f, 0.1f};
+
+GLuint lightIndices[] = {
+    0, 1, 2,
+    0, 2, 3,
+    0, 4, 7,
+    0, 7, 3,
+    3, 7, 6,
+    3, 6, 2,
+    2, 6, 5,
+    2, 5, 1,
+    1, 5, 4,
+    1, 4, 0,
+    4, 5, 6,
+    4, 6, 7};
 
 int main(int argc, char const *argv[])
 {
@@ -87,13 +128,47 @@ int main(int argc, char const *argv[])
     ElementBufferObject ebo(indices, sizeof(indices));
 
     // Link the attributes to the VAO
-    vao.LinkAttrib(vbo, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);                   // Vertex position
-    vao.LinkAttrib(vbo, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float))); // Vertex color
-    vao.LinkAttrib(vbo, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float))); // Vertex Texture coordinate
+    vao.LinkAttrib(vbo, 0, 3, GL_FLOAT, 11 * sizeof(float), (void *)0);                   // Vertex position
+    vao.LinkAttrib(vbo, 1, 3, GL_FLOAT, 11 * sizeof(float), (void *)(3 * sizeof(float))); // Vertex color
+    vao.LinkAttrib(vbo, 2, 2, GL_FLOAT, 11 * sizeof(float), (void *)(6 * sizeof(float))); // Vertex Texture coordinate
+    vao.LinkAttrib(vbo, 3, 3, GL_FLOAT, 11 * sizeof(float), (void *)(8 * sizeof(float))); // Face Normals
     // Unbind all to prevent modifying
     vao.Unbind();
     vbo.Unbind();
     ebo.Unbind();
+
+    // Shader for light cube
+    Shader lightShader("../shader/LightShader.vertexshader", "../shader/LightShader.fragmentshader");
+    // Generates Vertex Array Object and binds it
+    VertexArrayObject lightVAO;
+    lightVAO.Bind();
+    // Generates Vertex Buffer Object and links it to vertices
+    VertexBufferObject lightVBO(lightVertices, sizeof(lightVertices));
+    // Generates Element Buffer Object and links it to indices
+    ElementBufferObject lightEBO(lightIndices, sizeof(lightIndices));
+    // Links VBO attributes such as coordinates and colors to VAO
+    lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void *)0);
+    // Unbind all to prevent accidentally modifying them
+    lightVAO.Unbind();
+    lightVBO.Unbind();
+    lightEBO.Unbind();
+
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel, lightPos);
+
+    glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::mat4 pyramidModel = glm::mat4(1.0f);
+    pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+    lightShader.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+    glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    shaderProgram.Activate();
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+    glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
     // Texture
     Texture tex0("../assets/textures/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
@@ -120,6 +195,7 @@ int main(int argc, char const *argv[])
 
         // Use the shader program
         shaderProgram.Activate();
+        glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.position.x, camera.position.y, camera.position.z);
         // Export the camMatrix to the Shader
         camera.Matrix(shaderProgram, "camMatrix");
         // Bind the Texture
@@ -128,6 +204,15 @@ int main(int argc, char const *argv[])
         vao.Bind();
         // Draw the Triangles
         glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+        // Use the shader program
+        lightShader.Activate();
+        // Export the camMatrix to the Shader
+        camera.Matrix(lightShader, "camMatrix");
+        // Bind the VAO
+        lightVAO.Bind();
+        // Draw primitives, number of indices, datatype of indices, index of indices
+        glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
         // Swap buffers
         glfwSwapBuffers(window);
@@ -152,6 +237,10 @@ int main(int argc, char const *argv[])
     ebo.Delete();
     tex0.Delete();
     shaderProgram.Delete();
+    lightVAO.Delete();
+    lightVBO.Delete();
+    lightEBO.Delete();
+    lightShader.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
